29d4ffa60fc8fe714fc20730b83b151a8cf3e3db
sunandav <sunandavada@gmail.com>
2025-12-28 22:16:03 +0530

    Revert "After Tasks Completion"
    This reverts commit 41d99d887fbaca39780a98fcd75c1ee33b035987.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,6 @@ public class Main {
     // Task 1: create a class Product inside Main class
   static class Product {
 
-
         // Task 3
         public int product(int x, int y) {
             return x * y;
